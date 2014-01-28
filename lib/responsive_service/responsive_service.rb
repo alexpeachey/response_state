0c89107c39aae890fd39cc9448401f52f0d278bb
alexpeachey <alex.peachey@gmail.com>
2014-01-28 14:23:43 -0800

    Add responder_factory to ResonsiveService

--- lib/responsive_service/responsive_service.rb
+++ lib/responsive_service/responsive_service.rb
@@ -1,7 +1,13 @@
 module ResponsiveService
   class ResponsiveService
+    attr_reader :responder_factory
+
+    def initialize(responder_factory=ResponsiveService::Responder)
+      @responder_factory = responder_factory
+    end
+
     def call(&block)
-      yield Responder.new(:unimplemented, "A ResponsiveService should implement the call method.\nThe call method should perform the relevant work of the service and yield a ResponsiveService::Responder object.\n")
+      yield responder_factory.new(:unimplemented, "A ResponsiveService should implement the call method.\nThe call method should perform the relevant work of the service and yield a ResponsiveService::Responder object.\n")
     end
   end
 end
--- spec/lib/responsive_service/responsive_service_spec.rb
+++ spec/lib/responsive_service/responsive_service_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 module ResponsiveService
   describe ResponsiveService do
-    subject(:service) { ResponsiveService.new }
+    subject(:service) { ResponsiveService.new(responder_factory) }
+    let(:responder_factory) { double :responder_factory, new: nil }
+    let(:responder) { double :responder }
 
     describe '#call' do
+      before { responder_factory.stub(:new).with(:unimplemented, "A ResponsiveService should implement the call method.\nThe call method should perform the relevant work of the service and yield a ResponsiveService::Responder object.\n").and_return(responder) }
       it 'yields with a responder indicating instructions' do
-        service.call do |response|
-          expect(response.type).to eq :unimplemented
-          expect(response.message).to eq "A ResponsiveService should implement the call method.\nThe call method should perform the relevant work of the service and yield a ResponsiveService::Responder object.\n"
-        end
+        expect { |b| service.call(&b) }.to yield_with_args(responder)
       end
     end
   end
