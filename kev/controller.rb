464f5b8a0d4cc6a773b2a13319ec5d35433419ef
Kevin Goslar <kevin.goslar@gmail.com>
2015-10-02 19:27:59 -0700

    Remove spike

--- kev/controller.rb
+++
@@ -1,8 +0,0 @@
-require './user_service'
-
-
-create_user 1 do |result|
-  result.success   { puts "responding with 200" }
-  result.not_found { puts "responding with 404" }
-  result.missing_data { puts "responding with 400" }
-end
