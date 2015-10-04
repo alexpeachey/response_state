688a7cffd960af2f540b557ce097fc973e8588a6
Kevin Goslar <kevin.goslar@gmail.com>
2015-10-04 07:16:21 -0700

    Make define_instance_method private

--- lib/response_state.rb
+++ lib/response_state.rb
@@ -9,12 +9,6 @@ class ResponseState
     end
   end
 
-  def define_instance_method name, &block
-    (class << self; self; end).class_eval do
-      define_method name, &block
-    end
-  end
-
 
   def method_missing name, *args, &block
     if @status == :registration
@@ -28,6 +22,15 @@ private
   def initialize allowed_states
     @status = :registration
     @allowed_states = allowed_states
+  end
+
+
+  # Adds a method with the given name and body only to the current instance,
+  # not the class itself.
+  def define_instance_method name, &block
+    (class << self; self; end).class_eval do
+      define_method name, &block
+    end
   end
 
 end
