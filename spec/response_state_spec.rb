b9164858db327d3b8d1e80826def33bf518d6579
Kevin Goslar <kevin.goslar@gmail.com>
2015-10-04 07:13:21 -0700

    Inline examples

--- spec/response_state_spec.rb
+++ spec/response_state_spec.rb
@@ -1,36 +1,58 @@
 require 'spec_helper'
 
-# An example service that creates a new user.
-# Signals its results via a ResponseState.
-def create_user param, &block
-  result = ResponseState.init(&block)
-  if param == 1
-    result.success
-  elsif param == 2
-    result.success
-    result.success
-  else
-    result.failure
-  end
-end
 
 
 describe ResponseState do
 
-  it 'calls the state handler for the given response' do
-    called = false
-    create_user 1 do |result|
-      result.success   { called = true }
-      result.failure { fail }
+  context 'without allowed states specified' do
+
+    before do
+      def create_user param, &block
+        result = ResponseState.init(&block)
+        if param == 1
+          result.success
+        elsif param == 2
+          result.success
+          result.success
+        else
+          result.failure
+        end
+      end
     end
-    expect(called).to be true
+
+    it 'calls the state handler for the given response' do
+      called = false
+      create_user 1 do |result|
+        result.success   { called = true }
+        result.failure { fail }
+      end
+      expect(called).to be true
+    end
+
+    it 'can call state handlers multiple times' do
+      call_count = 0
+      create_user 2 do |result|
+        result.success   { call_count += 1 }
+        result.failure { fail }
+      end
+      expect(call_count).to eq 2
+    end
+
+    it 'tolerates not subscribing to some states that are returned' do
+      create_user 1 do |result|
+        result.zonk   {fail }
+      end
+    end
+
+    it 'tolerates subscribing to a non-existing state' do
+      create_user 1 do |result|
+        result.zonk   {fail }
+      end
+    end
   end
 
-  it 'can call state handlers multiple times' do
-    call_count = 0
-    create_user 2 do |result|
-      result.success   { call_count += 1 }
-      result.failure { fail }
+
+
     end
     expect(call_count).to eq 2
   end
